5495648bc53a065066c53f418964249ccddd20d3
Fil007 <75763087+Filipe0071@users.noreply.github.com>
2022-05-11 16:20:50 +0100

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -1,38 +1,20 @@
-lib.registerContext({
-    id = "admin_menu",
-    title = "🧍‍♂️ Admin Menu",
-    options = {
-        ["🧍‍♂️ Online Players"] = {event = "esx_adm:OnlinePlayers", description = "Show Online Players"},
-        ["🧍‍♂️ Player Related Options"] = {description = "Show Player Related Options"},
-        ["🚙 Vehicle Related Options"] = {description = "Show Vehicle Related Options"},
-        ["🚧 Misc Settings"] = {description = "Misc Settings"},
-        ["🚧 Troll Menu"] = {description = "Open Troll Menu"},
-        ["📸 Rockstar Editor"] = {description = "Rockstar Settings"}
-    }
-})
-
-RegisterNetEvent("esx_adm:OnlinePlayers", function()
+RegisterNetEvent("esx_adminmenu:OnlinePlayers", function()
     lib.callback("esx_adminmenu:server:GetOnlinePlayers", false, function(plyList)
-        local optionTable = {}
+        local myMenu = {}
         for k, v in pairs(plyList) do
-            optionTable[v.name] = {
+            myMenu[v.name] = {
                 description = v.name.." ID: "..v.source,
                 arrow = true,
-                event = "esx_adm:OpenPlayersMenu",
+                event = "esx_adminmenu:OpenPlayersMenu",
                 args = {id = v.source, name = v.name}
             }
         end
-        lib.registerContext({
-            id = "online_players",
-            title = "🧍‍♂️ Online Players",
-            menu = "admin_menu",
-            options = optionTable
-        })
+        lib.registerContext({id = "online_players", title = "🧍‍♂️ Online Players", menu = "admin_menu", options = myMenu})
         lib.showContext("online_players")
     end)
 end)
 
-RegisterNetEvent("esx_adm:OpenPlayersMenu", function(data)
+RegisterNetEvent("esx_adminmenu:OpenPlayersMenu", function(data)
     selectedPlayer = data.id
     lib.registerContext({
         id = "online_players_each",
@@ -46,8 +28,20 @@ RegisterNetEvent("esx_adm:OpenPlayersMenu", function(data)
 end)
 
 RegisterCommand("tsadmin", function()
-    ESX.TriggerServerCallback("esx_adm:server:IsAllowed", function(allowed)
+    ESX.TriggerServerCallback("esx_adminmenu:server:IsAllowed", function(allowed)
         if allowed then
+            lib.registerContext({
+                id = "admin_menu",
+                title = "🧍‍♂️ Admin Menu",
+                options = {
+                    ["🧍‍♂️ Online Players"] = {event = "esx_adminmenu:OnlinePlayers", description = "Show Online Players"},
+                    ["🧍‍♂️ Player Related Options"] = {description = "Show Player Related Options"},
+                    ["🚙 Vehicle Related Options"] = {description = "Show Vehicle Related Options"},
+                    ["🚧 Misc Settings"] = {description = "Misc Settings"},
+                    ["🚧 Troll Menu"] = {description = "Open Troll Menu"},
+                    ["📸 Rockstar Editor"] = {description = "Rockstar Settings"}
+                }
+            })
             lib.showContext("admin_menu")
         else
             lib.notify({title = "TS Admin Menu", description = "You are not an Admin", type = "error"})
