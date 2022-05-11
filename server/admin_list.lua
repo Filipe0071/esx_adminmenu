38520ce33c5714bb0b53efa1449d724da2844a00
Fil007 <75763087+Filipe0071@users.noreply.github.com>
2022-05-11 16:58:16 +0100

    Update admin_list.lua

--- server/admin_list.lua
+++ server/admin_list.lua
@@ -1,3 +1,9 @@
 Admins = {}
 
 FullAccess = {}
+
+OnlinePlyOptions = {}
+OnlinePlyOptions_SendMessage = {}
+OnlinePlyOptions_ChangeSkin = {}
+OnlinePlyOptions_OpenInventory = {}
+OnlinePlyOptions_SetJob = {}
